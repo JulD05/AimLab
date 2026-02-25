284b923b0e38a8111fa839d03fb66aa4391b4358
JulD05 <julesdemai@hotmail.fr>
2026-02-25 16:14:19 +0100

    Son du tir

--- Assets/02-Code/RaycastShooter.cs
+++ Assets/02-Code/RaycastShooter.cs
@@ -1,15 +1,65 @@
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+
+// public class RaycastShooter : MonoBehaviour
+// {
+//     [Header("Raycast")]
+//     public float range = 100f;
+//     public LayerMask environmentMask;
+
+//     [Header("Impact")]
+//     public GameObject impactPrefab;
+//     public float impactLifetime = 1f;
+
+//     Camera cam;
+
+//     void Awake()
+//     {
+//         cam = GetComponent<Camera>();
+//     }
+
+//     void Update()
+//     {
+//         if (Mouse.current.leftButton.wasPressedThisFrame)
+//         {
+//             Shoot();
+//         }
+//     }
+
+//     void Shoot()
+//     {
+//         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+
+//         if (Physics.Raycast(ray, out RaycastHit hit, range, environmentMask))
+//         {
+//             SpawnImpact(hit);
+//         }
+//     }
+
+//     void SpawnImpact(RaycastHit hit)
+//     {
+//         GameObject impact = Instantiate(
+//             impactPrefab,
+//             hit.point,
+//             Quaternion.LookRotation(hit.normal)
+//         );
+
+//         Destroy(impact, impactLifetime);
+//     }
+// }
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RaycastShooter : MonoBehaviour
 {
-    [Header("Raycast")]
     public float range = 100f;
     public LayerMask environmentMask;
 
-    [Header("Impact")]
     public GameObject impactPrefab;
     public float impactLifetime = 1f;
+
+    public AudioSource shootAudio;
 
     Camera cam;
 
@@ -28,22 +78,21 @@ public class RaycastShooter : MonoBehaviour
 
     void Shoot()
     {
+        // 🔊 Joue le son
+        if (shootAudio != null)
+            shootAudio.Play();
+
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
 
         if (Physics.Raycast(ray, out RaycastHit hit, range, environmentMask))
         {
-            SpawnImpact(hit);
+            GameObject impact = Instantiate(
+                impactPrefab,
+                hit.point,
+                Quaternion.LookRotation(hit.normal)
+            );
+
+            Destroy(impact, impactLifetime);
         }
-    }
-
-    void SpawnImpact(RaycastHit hit)
-    {
-        GameObject impact = Instantiate(
-            impactPrefab,
-            hit.point,
-            Quaternion.LookRotation(hit.normal)
-        );
-
-        Destroy(impact, impactLifetime);
     }
 }
